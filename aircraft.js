38c48a7f374066322b7f8ce64ece493f8fa8ea1a
moralesalberto <alberto@moralitos.com>
2014-06-28 12:42:57 -0400

    starting ...

---
+++ aircraft.js
@@ -0,0 +1,4 @@
+var Aircraft = function(type, size) {
+  this.type = type
+  this.size = size
+};
--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 // Using require.js to load the necessary files in here
 
-require(['jquery', 'underscore', 'underscore.string', 'haml', 'aircraft', 'queue'], function(jquery, underscore, underscore_string, haml, aircraft, queue) {
+require(['jquery'], function(jquery) {
   $(document).ready(function() {
-    alert('here');
     // compile all the haml files
-    fn = haml.haml.compileHaml({ sourceId: 'main_body'});
+    fn = haml.compileHaml({ sourceId: 'main-body'});
 
     // paint the haml file in the main area div
-    $('#main-content').html('testing');
-    alert('here');
+    $('#main-content').html(fn());
 
   });
 });
